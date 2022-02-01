aabe7653640b22a91b3cae6eedc884bef3dc4225
Jang-Jaewon <jewon119@gmail.com>
2022-02-01 22:49:33 +0900

    Add: Use ListCreateAPIView and add user fields

--- instagram/models.py
+++ instagram/models.py
@@ -1,8 +1,10 @@
-from django.db import models
+from django.conf import settings
+from django.db   import models
 
 from core.models import TimeStamp
 
 
 class Post(TimeStamp):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     
--- instagram/views.py
+++ instagram/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework import generics
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import PostSerializer
 from .models      import Post
+
+class PublicPostListAPIView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
 class PostViewSet(ModelViewSet):
