529d14160e210042ecf6175390049d85803d1221
Jang-Jaewon <jewon119@gmail.com>
2022-02-03 21:30:23 +0900

    Modify: Using ModelViewSet for CRUD and using Router

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,9 @@
+from crypt import methods
 from django.shortcuts import get_object_or_404
 
 from rest_framework            import generics
 from rest_framework.viewsets   import ModelViewSet
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.views      import APIView
 from rest_framework.response   import Response
 
@@ -13,6 +14,20 @@ from .models      import Post
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    @action(detail=False, methods=['GET'])
+    def public(self, request):
+        qs = self.get_queryset().filter(is_public=True)
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['PATCH'])
+    def set_public(self, request, pk):
+        instance = self.get_object()
+        instance.is_public = True
+        instance.save(update_fields=['is_public'])
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
 # class PublicPostListAPIView(generics.ListAPIView):
 #     queryset = Post.objects.filter(is_public=True)
@@ -26,39 +41,39 @@ class PostViewSet(ModelViewSet):
 #     return Response(serializer.data)
 
 
-class PostListAPIView(APIView):
-    def get(self, request):
-        qs = Post.objects.filter(is_public=True)
-        serializer = PostSerializer(qs, many=True)
-        return Response(serializer.data)
+# class PostListAPIView(APIView):
+#     def get(self, request):
+#         qs = Post.objects.filter(is_public=True)
+#         serializer = PostSerializer(qs, many=True)
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save(author=request.user)
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
 
-public_post_list = PostListAPIView.as_view()
+# public_post_list = PostListAPIView.as_view()
 
 
-class PostDetailAPIView(APIView):
-    def get_object(self, pk):
-        return get_object_or_404(Post, pk=pk)
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def delete(self, request, pk):
-        post = self.get_objects(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class PostDetailAPIView(APIView):
+#     def get_object(self, pk):
+#         return get_object_or_404(Post, pk=pk)
+#     def get(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#     def put(self, request, pk):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def delete(self, request, pk):
+#         post = self.get_objects(pk)
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-public_post_detail = PostDetailAPIView.as_view()        
+# public_post_detail = PostDetailAPIView.as_view()        
