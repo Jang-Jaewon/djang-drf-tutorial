68c0d1196c63b2207f268dac7dd8816dab7d2b75
Jang-Jaewon <jewon119@gmail.com>
2022-02-06 16:36:29 +0900

    Add: Apply Authentication and Permission in DRF

--- instagram/views.py
+++ instagram/views.py
@@ -1,14 +1,16 @@
 from crypt import methods
 from django.shortcuts import get_object_or_404
 
-from rest_framework            import generics
-from rest_framework.generics   import RetrieveAPIView
-from rest_framework.renderers  import TemplateHTMLRenderer
-from rest_framework.viewsets   import ModelViewSet
-from rest_framework.decorators import api_view, action
-from rest_framework.views      import APIView
-from rest_framework.response   import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework             import generics
+from rest_framework.generics    import RetrieveAPIView
+from rest_framework.renderers   import TemplateHTMLRenderer
+from rest_framework.viewsets    import ModelViewSet
+from rest_framework.decorators  import api_view, action
+from rest_framework.views       import APIView
+from rest_framework.response    import Response
 
+from .permissions import IsAuthorOrReadonly
 from .serializers import PostSerializer
 from .models      import Post
 
@@ -16,6 +18,7 @@ from .models      import Post
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadonly]
 
     def perform_create(self, serializer):
         author = self.request.user
