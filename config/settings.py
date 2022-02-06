2e29f5ea836d0d92d9b5f2762c51ccff7e5aaf21
Jang-Jaewon <jewon119@gmail.com>
2022-02-06 18:27:46 +0900

    Modify: Use PageNumberPagination to apply Pagiation across the globe

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
